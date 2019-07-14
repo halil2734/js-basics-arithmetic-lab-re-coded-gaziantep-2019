ed6356499a69bb5576c9f6ee39b6513bef877d0b
Halil güler <h.guler3717@gmail.com>
2019-07-14 14:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your code here
 
-let var1 = 62;
-let var2 = 1;
+let var1 = 31;
+let var2 = 2;
 let multiply = var1 * var2;
 
-let random = Math.floor(Math.random()*10)
+let random = Math.floor(Math.random()*10);
+
+let mod = 
